6a63b6ebd28b149160fbe9808714daffb64c100a
dbrito <dbrito@segware.com>
2025-05-14 21:51:55 -0300

    feat: Many-to-many association with JoinTable

--- src/main/java/com/educandoWeb/course/config/TestConfig.java
+++ src/main/java/com/educandoWeb/course/config/TestConfig.java
@@ -27,10 +27,10 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 
@@ -46,22 +46,28 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		this.orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
-		Category cat1 = new Category(null, "Electronics"); 
-		Category cat2 = new Category(null, "Books"); 
-		Category cat3 = new Category(null, "Computers"); 
-		
-		this.categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		
-		
-		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
-		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
-		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
-		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
-		
-		this.productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		this.categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		this.productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategories().add(cat2);
+		p2.getCategories().addAll(Arrays.asList(cat1, cat3));
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		this.productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 
 }
