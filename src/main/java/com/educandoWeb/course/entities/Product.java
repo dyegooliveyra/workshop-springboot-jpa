13a993f8b986b68e302c53ce1b80604bf58b695b
dbrito <dbrito@segware.com>
2025-05-15 14:33:09 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoWeb/course/entities/Product.java
+++ src/main/java/com/educandoWeb/course/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +34,9 @@ public class Product implements Serializable {
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
+	@OneToMany(mappedBy = "id.product", fetch = FetchType.EAGER)
+	private Set<OrderItem> items = new HashSet<>();
+
 	public Product() {
 	}
 
@@ -41,6 +47,15 @@ public class Product implements Serializable {
 		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
+	}
+
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> orders = new HashSet<>();
+		for(OrderItem orderItem : items) {
+			orders.add(orderItem.gerOrder());
+		}
+		return orders;
 	}
 
 	public Long getId() {
